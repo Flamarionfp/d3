a1aa4557936fb595d176f2fd2450b56a246bb4f9
flamarionfp <flamarionpinto@sou.faccat.br>
2023-05-30 20:16:09 -0300

    chore: install d3 types

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
+import * as d3 from "d3";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="font-bold text-5xl text-red-500">Hello D3</h1>
-    </main>
+    <main className="bg-gray-900 text-white flex min-h-screen flex-col items-center p-24"></main>
   );
 }
